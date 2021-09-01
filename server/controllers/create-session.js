7e999c50781f8cc76f06702a00a49fdecc5972e4
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-09-01 19:36:14 +0300

    edit create session function Rlattes #10

--- server/controllers/create-session.js
+++ server/controllers/create-session.js
@@ -1,10 +1,9 @@
 const { sign } = require('jsonwebtoken');
 require('env2')('.env');
 
-const createSession = (userName, email) => {
+const createSession = (userName) => {
   const cookiePayload = {
     userName,
-    email,
   };
   const cookie = sign(cookiePayload, process.env.SECRET_KEY);
   return cookie;
