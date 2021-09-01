5af0b8645b90ca932316fceaa4bdd40d5415c375
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-09-01 19:36:57 +0300

    create login controller Relates #10

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,10 +1,24 @@
 const createSession = require('./create-session');
+const { comparePasswords } = require('./hashPassword');
+const { getUser } = require('../database/queries');
 
 const login = (req, res) => {
-  const { username, email } = req.body;
-  res.cookie('token', createSession(username, email), { httpOnly: true, secure: true });
-  res.cookie('username', username);
-  res.redirect('/');
+  const { username, password } = req.body;
+  getUser(username)
+    .then((row) => {
+      comparePasswords(password, row.password, (err, data) => {
+        if (err) {
+          res.status(500).json({ msg: 'internal server error' });
+        } else if (data) {
+          res.cookie('token', createSession(username), { httpOnly: true, secure: true });
+          res.cookie('username', username);
+          res.redirect('/');
+        } else res.json({ msg: 'invalid password' });
+      });
+    }).catch((err) => {
+      console.log(err);
+      res.json({ msg: 'error' });
+    });
 };
 
 module.exports = login;
