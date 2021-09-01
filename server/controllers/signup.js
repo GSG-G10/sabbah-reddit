532802cb38ae67f935a5d2d66ea1709399e30878
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-09-01 19:39:53 +0300

    create signup controller Relates #10

--- server/controllers/signup.js
+++ server/controllers/signup.js
@@ -1,14 +1,12 @@
-const { sign } = require('jsonwebtoken');
 const { addUserQu } = require('../database/queries');
 const { hashPassword } = require('./hashPassword');
-const {createSession} = require('./index')
+const { createSession } = require('./index');
 
 const createUser = (req, res) => {
   const {
     username, firstName, lastName, email, password,
   } = req.body;
   hashPassword(password).then((hashedPass) => {
-    console.log(hashedPass);
     const userObj = {
       username,
       firstName,
@@ -19,14 +17,12 @@ const createUser = (req, res) => {
     return userObj;
   })
     .then((obj) => {
-      console.log(obj);
       addUserQu(obj)
         .then(() => {
           res.cookie('token', createSession(username, email), { httpOnly: true, secure: true });
           res.cookie('username', username);
           res.redirect('/');
         }).catch((err) => {
-          console.log(err.message);
           res.status(500).json(err.message);
         });
     }).catch((err) => res.json(err.message));
